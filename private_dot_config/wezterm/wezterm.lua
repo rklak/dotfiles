dac629dcf6afd13836f28096ba848b5ffc449ea6
Radosław Kłak <radek.klak@gmail.com>
2024-08-08 14:18:10 +0200

    feat(macos): for macos support

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,7 +1,24 @@
 local wezterm = require("wezterm")
 
-return {
-	font = wezterm.font("Fira Code"),
+-- Function to check if the operating system is Linux
+local function is_linux()
+	local handle = io.popen("uname")
+	local result = handle:read("*a"):gsub("%s+", "") -- Remove extra whitespace
+	handle:close()
+	return result == "Linux"
+end
+
+-- Set the desired and fallback fonts
+local desired_font = "Fira Code"
+local fallback_font = "FiraCode Nerd Font"
+
+-- Determine which font to use based on the operating system
+local font_to_use = is_linux() and desired_font or fallback_font
+
+-- WezTerm configuration
+local config = {
+	font = wezterm.font(font_to_use),
+	font_size = 16,
 	color_scheme = "Firefly Traditional",
 	enable_tab_bar = false,
 	window_background_opacity = 0.85,
@@ -15,3 +32,5 @@ return {
 		},
 	},
 }
+
+return config
